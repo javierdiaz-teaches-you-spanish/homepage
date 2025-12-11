c774b87f26207926519674867eeb8cf578493612
javierdiaz-teaches-you-spanish <jjdiazb@yahoo.com>
2025-12-11 12:11:35 -0500

    configure target:web and browser child-process false

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
+  target: 'web',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
